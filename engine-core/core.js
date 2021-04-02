269dde0d957c402f6c18daab07a477d1a58ae1a2
danzyxr <49037253+danzyxr@users.noreply.github.com>
2021-04-02 16:27:01 -0500

    Update TODO

--- engine-core/core.js
+++ engine-core/core.js
@@ -1,10 +1,20 @@
+// Namespace engine
 var engine = engine || {};
+
+/*
+
+TODO:
+  Convert IIFE to ES6+
+
+*/
 
 engine.core = (function () {
   const objs = [];
   const canvas = document.getElementById('canvas');
+
   canvas.width = 800;
   canvas.height = 450;
+
   const public = {
     objs: objs,
     width: canvas.width,
--- rigid-bodies/RigidShape.js
+++ rigid-bodies/RigidShape.js
@@ -1,6 +1,6 @@
 class RigidShape {
-  constructor(angle = 0, center = new Vector2d(0, 0)) {
-    this.angle = angle;
+  constructor(center = new Vector2d(0, 0)) {
+    this.angle = 0;
     this.center = center;
     engine.core.objs.push(this);
   }
